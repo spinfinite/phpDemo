421ba16736f271b94c3c69a18a90e2552afbf1c8
Sergio <spinfinite07@gmail.com>
2023-11-07 10:25:12 -0700

    php code

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    // $name = "yoshi";
+    $age = 30;
+
+    // $name = "mario"
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title>my first PHP file</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Hello, Ninjas";
+        ?>
+    </h1>    
+
+    <div>
+        <?php echo $name ?>
+        <?php echo $age ?>
+    </div>
+</body>
+</html>
