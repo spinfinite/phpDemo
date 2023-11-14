d5ec6c9f40bbc845842bb37d3d94323fa291e7f0
Sergio <spinfinite07@gmail.com>
2023-11-14 09:29:04 -0700

    Associate Arrays and use of pop to take out the last item of the array

--- index.php
+++ index.php
@@ -116,13 +116,29 @@ $ninjasThree = array_merge($ninjasOne, $ninjasTwo);
 
 // Multi-Dimensional Arrays
 
-$blogs = [
-    ['mario party', 'mario', 'lorem', 30],
-    ['mario kart cheats', 'toad', 'lorem', 25],
-    ['zelda hidden chests', 'link', 'lorem', 50]
-];
+// $blogs = [
+//     ['mario party', 'mario', 'lorem', 30],
+//     ['mario kart cheats', 'toad', 'lorem', 25],
+//     ['zelda hidden chests', 'link', 'lorem', 50]
+// ];
 
 print_r($blogs[2][3]);
+
+// Associative Arrays
+$blogs = [
+    ['title' => 'mario party', 'author' => 'mario', 'content' => 'lorem', 'likes' => 30],
+    ['title' => 'mario kart cheats', 'author' => 'toad', 'content' => 'lorem', 'likes' => 25],
+    ['title' => 'zelda hidden chests', 'author' => 'link', 'content' => 'lorem', 'likes' => 50]
+];
+
+echo $blogs[2]['author'];
+echo count($blogs);
+$blogs[] = ['title' => 'castle party', 'author' => 'peach', 'content' => 'lorem', 'likes' => 100];
+print_r($blogs);
+
+// Remove item from array
+$popped = array_pop($blogs);
+print_r($popped);
 
 ?>
     </div>
